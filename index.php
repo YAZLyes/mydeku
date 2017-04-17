9049d916cbedacfbf0d3db5e7537b188f4e79d30
YAZLyes <1285812783@qq.com>
2017-04-17 10:18:52 +0800

    提交了index.php文件
    测试一下github

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>作业1</title>
+	</head>
+	<body>
+<h1>我是第一次</ h1>
+	</body>
+	<script type="text/javascript">
+
+	</script>
+</html>
